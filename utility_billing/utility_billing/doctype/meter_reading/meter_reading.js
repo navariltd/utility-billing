b5d5c37531d0a97fa05361d1af6b060375cb113a
muruthigitau <muruthigitau@gmail.com>
2024-10-03 17:00:17 +0300

    feat:Meter reading creation flow

--- utility_billing/utility_billing/doctype/meter_reading/meter_reading.js
+++ utility_billing/utility_billing/doctype/meter_reading/meter_reading.js
@@ -2,38 +2,68 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Meter Reading", {
-	// Trigger when the form is loaded or refreshed
 	onload: function (frm) {
-		// Auto-fill the Date field with the current date if it's empty
 		if (!frm.doc.date) {
 			frm.set_value("date", frappe.datetime.now_date());
 		}
 	},
 
-	// Trigger when the Customer is selected
 	customer: function (frm) {
 		if (frm.doc.customer) {
-			// Fetch Customer Name
-			frappe.db.get_value("Customer", frm.doc.customer, "customer_name", (r) => {
-				frm.set_value("customer_name", r.customer_name);
+			frappe.call({
+				method: "utility_billing.utility_billing.doctype.meter_reading.meter_reading.get_customer_details",
+				args: {
+					customer: frm.doc.customer,
+				},
+				callback: function (r) {
+					if (r.message) {
+						frm.set_value("customer_name", r.message.customer_name);
+						frm.set_value("price_list", r.message.default_price_list);
+					}
+				},
 			});
+		}
+	},
 
-			// Fetch Price List (from customer or customer group)
-			frappe.db.get_value("Customer", frm.doc.customer, "default_price_list", (r) => {
-				if (r.default_price_list) {
-					frm.set_value("price_list", r.default_price_list);
-				} else {
-					// If no price list on customer, check customer group
-					frappe.db.get_value(
-						"Customer Group",
-						frm.doc.customer_group,
-						"default_price_list",
-						(r) => {
-							frm.set_value("price_list", r.default_price_list);
-						}
-					);
-				}
+	items_add: function (frm) {
+		frm.doc.items.forEach((item) => {
+			if (item.tariff_table && item.tariff_table.length > 0) {
+				item.rate = 0;
+			}
+		});
+		frm.refresh_field("items");
+	},
+});
+
+frappe.ui.form.on("Meter Reading Item", {
+	meter_number: function (frm, cdt, cdn) {
+		const row = locals[cdt][cdn];
+		if (row.meter_number) {
+			frappe.call({
+				method: "utility_billing.utility_billing.doctype.meter_reading.meter_reading.get_previous_reading",
+				args: {
+					meter_number: row.meter_number,
+				},
+				callback: function (r) {
+					row.previous_reading = r.message || 0;
+					frm.refresh_field("items");
+				},
 			});
+		}
+	},
+
+	current_reading: function (frm, cdt, cdn) {
+		const row = locals[cdt][cdn];
+		if (row.previous_reading !== undefined || row.previous_reading !== 0) {
+			row.consumption = row.current_reading - row.previous_reading;
+			frm.refresh_field("items");
+		}
+	},
+	previous_reading: function (frm, cdt, cdn) {
+		const row = locals[cdt][cdn];
+		if (row.current_reading !== undefined || row.current_reading !== 0) {
+			row.consumption = row.current_reading - row.previous_reading;
+			frm.refresh_field("items");
 		}
 	},
 });
--- utility_billing/utility_billing/doctype/meter_reading/meter_reading.py
+++ utility_billing/utility_billing/doctype/meter_reading/meter_reading.py
@@ -2,60 +2,90 @@
 # For license information, please see license.txt
 import frappe
 from frappe.model.document import Document
+from frappe.utils import nowdate
 
 
 class MeterReading(Document):
+    def validate(self):
+        settings = frappe.get_single("Utility Billing Settings")
+        if settings.sales_order_creation_state == "Draft":
+            create_sales_order(self)
+
+    def after_insert(self):
+        create_meter_reading_rates(self)
+
     def before_submit(self):
-        for item in self.items:
-            # Fetch previous reading
-            previous_reading = frappe.db.get_value(
-                "Meter Reading Item",
-                {"item_code": item.item_code, "customer": self.customer},
-                "current_reading",
-                order_by="modified desc",
-            )
-            item.previous_reading = previous_reading or 0
-
-            # Calculate consumption
-            item.consumption = item.current_reading - item.previous_reading
-
-            # Apply rates based on consumption
-            apply_meter_reading_rates(item)
+        settings = frappe.get_single("Utility Billing Settings")
+        if settings.sales_order_creation_state == "Submitted":
+            create_sales_order(self)
 
 
-def apply_meter_reading_rates(item):
-    # Tariff-based calculation logic
-    tariff_blocks = frappe.get_list(
-        "Item Price Tariff",
-        filters={"item_code": item.item_code},
-        order_by="lower_limit asc",
-    )
+def create_meter_reading_rates(meter_reading):
+    """Create Meter Reading Rate documents from the Meter Reading."""
+    for item in meter_reading.items:
+        existing_rates = frappe.get_list(
+            "Meter Reading Rate", filters={"meter_reading_item": item.name}
+        )
+        if existing_rates:
+            continue
 
-    remaining_consumption = item.consumption
-    for block in tariff_blocks:
-        rate = block.rate
-        lower_limit = block.lower_limit
-        upper_limit = block.upper_limit
-
-        # Determine quantity for this block
-        if remaining_consumption <= upper_limit:
-            quantity = remaining_consumption
-        else:
-            quantity = upper_limit - lower_limit
-
-        # Calculate amount for this block
+        rate = 1
+        quantity = 1
         amount = quantity * rate
-        # Create Meter Reading Rate entry
-        item.append(
-            "rates",
+        rate_doc = frappe.get_doc(
             {
+                "doctype": "Meter Reading Rate",
+                "meter_reading_item": item.name,
                 "item_code": item.item_code,
                 "quantity": quantity,
-                "rate": rate,
                 "amount": amount,
+                "rate": rate,
+                "meter_reading": meter_reading.name,
+            }
+        )
+        rate_doc.insert()
+
+
+def create_sales_order(meter_reading):
+    sales_order = frappe.get_doc(
+        {"doctype": "Sales Order", "customer": meter_reading.customer, "items": []}
+    )
+    for item in meter_reading.items:
+        sales_order.append(
+            "items",
+            {
+                "item_code": item.item_code,
+                "qty": item.consumption,
+                "delivery_date": nowdate(),
             },
         )
 
-        remaining_consumption -= quantity
-        if remaining_consumption <= 0:
-            break
+    sales_order.insert()
+
+
+@frappe.whitelist()
+def get_previous_reading(meter_number):
+    """Fetch all previous readings for the specified meter number using SQL."""
+    query = """
+        SELECT current_reading, creation
+        FROM `tabMeter Reading Item`
+        WHERE meter_number = %s
+        ORDER BY creation DESC
+    """
+    previous_readings = frappe.db.sql(query, (meter_number,), as_dict=True)
+    return previous_readings[0].current_reading if previous_readings else 0
+
+
+@frappe.whitelist()
+def get_customer_details(customer):
+    """Fetch customer name and price list."""
+    customer_details = frappe.db.get_value(
+        "Customer", customer, ["customer_name", "default_price_list"], as_dict=True
+    )
+    if customer_details:
+        if not customer_details.default_price_list:
+            customer_group = frappe.db.get_value("Customer", customer, "customer_group")
+            customer_details.default_price_list = frappe.db.get_value(
+                "Customer Group", customer_group, "default_price_list"
+            )
+    return customer_details or {}
