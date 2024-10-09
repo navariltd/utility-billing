023067be8577dbb1592917314888553a3d257796
muruthigitau <muruthigitau@gmail.com>
2024-10-09 16:23:07 +0300

    feat: Customer creation on submit - Request for Utility Service

--- utility_billing/utility_billing/doctype/request_for_utility_service/request_for_utility_service.py
+++ utility_billing/utility_billing/doctype/request_for_utility_service/request_for_utility_service.py
@@ -1,12 +1,26 @@
 # Copyright (c) 2024, Navari and contributors
 # For license information, please see license.txt
 
+import frappe
 from frappe.contacts.address_and_contact import load_address_and_contact
-
-# import frappe
 from frappe.model.document import Document
 
 
 class RequestforUtilityService(Document):
     def onload(self):
         load_address_and_contact(self)
+
+    def on_submit(self):
+        if not frappe.db.exists("Customer", self.customer_name):
+            customer = frappe.get_doc(
+                {
+                    "doctype": "Customer",
+                    "customer_name": self.customer_name,
+                    "customer_type": self.customer_type,
+                    "customer_group": self.customer_group,
+                    "territory": self.territory,
+                    "nrcpassport_no": self.nrcpassport_no,
+                    "organisation_registration_no": self.organisation_registration_no,
+                }
+            )
+            customer.insert()
