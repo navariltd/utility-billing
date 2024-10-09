4b0408b14998e35401013603a96a8097c356b472
muruthigitau <muruthigitau@gmail.com>
2024-10-09 16:00:15 +0300

    feat: Contact & Address - Request for Utility Service

--- utility_billing/utility_billing/doctype/request_for_utility_service/request_for_utility_service.py
+++ utility_billing/utility_billing/doctype/request_for_utility_service/request_for_utility_service.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2024, Navari and contributors
 # For license information, please see license.txt
 
+from frappe.contacts.address_and_contact import load_address_and_contact
+
 # import frappe
 from frappe.model.document import Document
 
 
 class RequestforUtilityService(Document):
-    pass
+    def onload(self):
+        load_address_and_contact(self)
