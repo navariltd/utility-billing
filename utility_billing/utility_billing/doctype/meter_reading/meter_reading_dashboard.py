60d80155c888af9c5b0da366c609fec6e1041242
muruthigitau <muruthigitau@gmail.com>
2024-10-22 09:27:19 +0300

    feat: SalesInvoiceMeterReading & SalesOrderMeterReading childtables

--- utility_billing/utility_billing/doctype/meter_reading/meter_reading_dashboard.py
+++ utility_billing/utility_billing/doctype/meter_reading/meter_reading_dashboard.py
@@ -1,11 +1,16 @@
+from frappe import _
 def get_data():
     return {
-        "fieldname": "meter_reading",
+        "fieldname": "name", 
         "transactions": [
             {
+                "label": _("Related Documents"),
                 "items": [
                     "Sales Order",
                 ],
             },
         ],
+        "dynamic_links": {
+            "Sales Order": ["meter_readings", "meter_reading"]  
+        }
     }
---
+++ utility_billing/utility_billing/doctype/sales_invoice_meter_reading/sales_invoice_meter_reading.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Navari and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SalesInvoiceMeterReading(Document):
+	pass
