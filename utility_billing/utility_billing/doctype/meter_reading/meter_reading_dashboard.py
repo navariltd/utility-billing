011519aae388d5b4084dee380386d59bb6245e81
muruthigitau <muruthigitau@gmail.com>
2024-10-22 08:20:26 +0300

    feat: meter reading connection dashboard

---
+++ utility_billing/utility_billing/doctype/meter_reading/meter_reading_dashboard.py
@@ -0,0 +1,11 @@
+def get_data():
+    return {
+        "fieldname": "meter_reading",
+        "transactions": [
+            {
+                "items": [
+                    "Sales Order",
+                ],
+            },
+        ],
+    }
