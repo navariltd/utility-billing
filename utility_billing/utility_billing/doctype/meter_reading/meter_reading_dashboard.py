3dae6689dc9f33a5db1fb64f9041771f52324ef6
muruthigitau <muruthigitau@gmail.com>
2024-10-22 11:33:28 +0300

    fix: sales order creation from meter_reading

--- utility_billing/utility_billing/doctype/meter_reading/meter_reading_dashboard.py
+++ utility_billing/utility_billing/doctype/meter_reading/meter_reading_dashboard.py
@@ -1,16 +1,20 @@
 from frappe import _
+
+
 def get_data():
     return {
-        "fieldname": "name", 
+        "fieldname": "meter_reading",
+        "dynamic_links": {
+            "Sales Order": ["meter_readings", "meter_reading"],
+            "Sales Invoice": ["meter_readings", "meter_reading"],
+        },
         "transactions": [
             {
                 "label": _("Related Documents"),
                 "items": [
                     "Sales Order",
+                    "Sales Invoice",
                 ],
             },
         ],
-        "dynamic_links": {
-            "Sales Order": ["meter_readings", "meter_reading"]  
-        }
     }
--- utility_billing/utility_billing/doctype/sales_order_meter_reading/sales_order_meter_reading.py
+++ utility_billing/utility_billing/doctype/sales_order_meter_reading/sales_order_meter_reading.py
@@ -6,4 +6,4 @@ from frappe.model.document import Document
 
 
 class SalesOrderMeterReading(Document):
-	pass
+    pass
