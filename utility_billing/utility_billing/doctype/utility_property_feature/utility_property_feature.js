8989703dc3802a3f60e1841942f9dca97ba1ca76
muruthigitau <muruthigitau@gmail.com>
2025-04-28 15:08:50 +0300

    feat: add Utility Property Feature and related types with comprehensive fields

---
+++ utility_billing/utility_billing/doctype/utility_property_feature/utility_property_feature.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Navari and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Utility Property Feature", {
+// 	refresh(frm) {
+
+// 	},
+// });
